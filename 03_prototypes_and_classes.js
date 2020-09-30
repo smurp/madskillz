6df04adde7fac55aec22bea599933a08e2ea72c4
Shawn Murphy <smurp@smurp.com>
2020-09-30 17:28:58 +0200

    add examination of the use of new with constructors

--- 03_prototypes_and_classes.js
+++ 03_prototypes_and_classes.js
@@ -13,6 +13,7 @@ class SpecialSmileyFace extends FancySmileyFace { // subclass
   }
 }
 
+
 // OLD WAY 
 var SmileyFace = function() {}
 SmileyFace.prototype.draw = function() {}
@@ -27,3 +28,41 @@ var smileyRobert = new FancySmileyFace(3, 9);
 
 // IF WE DO NOT USE new WITH THE OLD WAY WE CAN STILL DO IT THE HARD WAY
 var smileyBobHardWay = SmileyFace.call({}, 3, 9); smileyBobHardWay.__proto__ = SmileyFace;
+
+// new REALLY DOES THIS
+var smileyBobHardWay = SmileyFace.call({__proto__: SmileyFace}, 3, 9);;
+
+var Flower = function(x, y, height){
+  // this = {}
+  this.x = x;
+  this.y = y;
+  this.height = height;
+}
+
+goodFlower = new Flower(3, 9, 12); // same as...
+goodFlower = Flower.call({__proto__: Flower}, 3, 9, 12);;
+goodFlower2 = new Flower(3, 9, 12); // same as...
+goodFlower == goodFlower2 // false
+
+// without new
+brokenFlower = Flower(2,8,11); // same as....
+brokenFlower = Flower.call(window, 2, 8, 11);
+brokenFlower2 = Flower(2,8,11);
+brokenFlower == brokenFlower2 // true
+brokenFlower == window // true
+
+
+
+class Flower {
+  constructor: (x, y, height) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+  }
+}
+
+class Tulip extends Flower {
+  constuctor: (x, y, height) {
+    super(x, y, height*x);
+  }
+}
